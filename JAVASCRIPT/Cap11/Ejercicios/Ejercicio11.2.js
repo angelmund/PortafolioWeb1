1ad16258b6a7a7771850d44fde353ff806e8f8a7
angelmund <97712695+angelmund@users.noreply.github.com>
2022-11-21 20:01:13 -0600

    Ejercicio11.3

---
+++ JAVASCRIPT/Cap11/Ejercicios/Ejercicio11.2.js
@@ -0,0 +1,20 @@
+
+/*  let hoy = new Date();
+
+let dia = hoy.getDate();
+let mes = hoy.getMonth() + 1;
+let angio = hoy.getFullYear();
+
+dia = ('0' + dia).slice(-2);
+mes = ('0' + mes).slice(-2);
+
+
+//formato de mostrar la fecha
+let formato = '${angio}-${mes}-${dia}';
+window.alert(formato);
+alert("Fecha")
+*/
+
+let date = new Date(Date.parse("2012-01-26T13:51:50.417-07:00"));
+
+alert(date);
